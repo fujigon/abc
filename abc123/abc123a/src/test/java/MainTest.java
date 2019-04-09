77838c157e9a4bc778f46c1da17c74bb89d6b5fd
Kazuhisa <dec12.kaz@gmail.com>
2019-04-10 01:45:33 +0900

    apply intelliJ google code style

--- abc123/abc123a/src/test/java/MainTest.java
+++ abc123/abc123a/src/test/java/MainTest.java
@@ -9,28 +9,28 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class MainTest {
 
-    @Test
-    public void test() {
-        String input = "";
-        input += "1 2 3 4 8 9 15" + System.lineSeparator();
+  @Test
+  public void test() {
+    String input = "";
+    input += "1 2 3 4 8 9 15" + System.lineSeparator();
 
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        Main.solve(
-                new ByteArrayInputStream(input.getBytes()), new PrintStream(os)
-        );
-        assertThat(os.toString(), is("Yay!" + System.lineSeparator()));
-    }
+    ByteArrayOutputStream os = new ByteArrayOutputStream();
+    Main.solve(
+        new ByteArrayInputStream(input.getBytes()), new PrintStream(os)
+    );
+    assertThat(os.toString(), is("Yay!" + System.lineSeparator()));
+  }
 
-    @Test
-    public void test2() {
-        String input = "";
-        input += "15 18 26 35 36 18" + System.lineSeparator();
+  @Test
+  public void test2() {
+    String input = "";
+    input += "15 18 26 35 36 18" + System.lineSeparator();
 
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        Main.solve(
-                new ByteArrayInputStream(input.getBytes()), new PrintStream(os)
-        );
-        assertThat(os.toString(), is(":(" + System.lineSeparator()));
-    }
+    ByteArrayOutputStream os = new ByteArrayOutputStream();
+    Main.solve(
+        new ByteArrayInputStream(input.getBytes()), new PrintStream(os)
+    );
+    assertThat(os.toString(), is(":(" + System.lineSeparator()));
+  }
 
 }
