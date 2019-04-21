be8c4365b49c11e809c310157486ccc0bbca262b
Kazuhisa <dec12.kaz@gmail.com>
2019-04-21 17:01:16 +0900

    A, B, C done D WIP

---
+++ abc122/d/src/test/java/MainTest.java
@@ -0,0 +1,46 @@
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MainTest {
+
+  @Test
+  public void test() {
+    String input = "3";
+
+    String expected = "61";
+
+    assertThat(execute(input + "\n"), is(expected + "\n"));
+  }
+
+  @Test
+  public void test2() {
+    String input = "4";
+
+    String expected = "230";
+
+    assertThat(execute(input + "\n"), is(expected + "\n"));
+  }
+
+  @Test
+  public void test3() {
+    String input = "100";
+
+    String expected = "388130742";
+
+    assertThat(execute(input + "\n"), is(expected + "\n"));
+  }
+
+  private String execute(String input) {
+    ByteArrayOutputStream os = new ByteArrayOutputStream();
+    Main.solve(
+        new ByteArrayInputStream(input.getBytes()), new PrintStream(os)
+    );
+    return os.toString().replace(System.lineSeparator(), "\n");
+  }
+}
