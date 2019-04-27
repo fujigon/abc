a6e13ff7ef7a7e7f3e1dd88a38ad94bddc477a55
Kazuhisa <dec12.kaz@gmail.com>
2019-04-27 20:49:27 +0900

    prepare

---
+++ abc125/src/test/java/abc125/d/MainTest.java
@@ -0,0 +1,51 @@
+package abc125.d;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MainTest {
+
+  @Test
+  public void test() {
+    String input = "5 1\n"
+        + "00010";
+
+    String expected = "4";
+
+    assertThat(execute(input + "\n"), is(expected + "\n"));
+  }
+
+  @Test
+  public void test2() {
+    String input = "14 2\n"
+        + "11101010110011";
+
+    String expected = "8";
+
+    assertThat(execute(input + "\n"), is(expected + "\n"));
+  }
+
+  @Test
+  public void test3() {
+    String input = "1 1\n"
+        + "1";
+
+    String expected = "1";
+
+    assertThat(execute(input + "\n"), is(expected + "\n"));
+  }
+
+  private String execute(String input) {
+    ByteArrayOutputStream os = new ByteArrayOutputStream();
+    Main.solve(
+        new ByteArrayInputStream(input.getBytes()), new PrintStream(os)
+    );
+    return os.toString().replace(System.lineSeparator(), "\n");
+  }
+}
