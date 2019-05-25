f5a24c334d4a45e53bec4f68d2c96e877c8cf5bf
kfujita <dec12.kaz@gmail.com>
2019-05-25 20:36:57 +0900

    prepare

---
+++ abc127/src/main/java/abc127/a/Main.java
@@ -0,0 +1,27 @@
+package abc127.a;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    solve(System.in, System.out);
+  }
+
+  static void solve(InputStream is, PrintStream os) {
+    Scanner sc = new Scanner(is);
+
+    /* read */
+
+    int n = sc.nextInt();
+    int k = sc.nextInt();
+
+    String s = sc.next();
+
+    String result = s.substring(0, k - 1) + s.substring(k - 1, k).toLowerCase() + s.substring(k, n);
+
+    os.println(result);
+  }
+}
---
+++ abc127/src/test/java/abc127/a/MainTest.java
@@ -0,0 +1,40 @@
+package abc127.a;
+
+import abc127.a.Main;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MainTest {
+
+  @Test
+  public void test() {
+    String input = "3 1\n"
+        + "ABC";
+    String expected = "aBC";
+
+    assertThat(execute(input + "\n"), is(expected + "\n"));
+  }
+
+  @Test
+  public void test1() {
+    String input = "3 3\n"
+        + "ABC";
+    String expected = "ABc";
+
+    assertThat(execute(input + "\n"), is(expected + "\n"));
+  }
+
+  private String execute(String input) {
+    ByteArrayOutputStream os = new ByteArrayOutputStream();
+    Main.solve(
+        new ByteArrayInputStream(input.getBytes()), new PrintStream(os)
+    );
+    return os.toString().replace(System.lineSeparator(), "\n");
+  }
+
+}
