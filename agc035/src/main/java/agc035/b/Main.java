6f9a6c64a9c7e275b22e6a4c272245bbbd89d285
kfujita <dec12.kaz@gmail.com>
2019-07-14 20:49:17 +0900

    prepare

---
+++ agc035/src/main/java/agc035/b/Main.java
@@ -0,0 +1,89 @@
+package agc035.b;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+
+  public static void main(String[] args) {
+    solve(System.in, System.out);
+  }
+
+  static void solve(InputStream is, PrintStream os) {
+    Scanner sc = new Scanner(is);
+
+    /* read */
+    String s = sc.next();
+
+    os.println(search(s));
+
+  }
+//  private static long search(String s) {
+//
+//    int n = s.length();
+//
+//    long sum = 0;
+//    int j = 0;
+//    while (0 <= j &&j < n){
+//
+//      if (s.charAt(j) != 'A') {
+//        j = s.indexOf("A", j + 1);
+//        continue;
+//      }
+//
+//      // for each, find A
+//      // found A
+//
+//      int aCount = 0;
+//      int i = j;
+//      while (i < n && (s.charAt(i) == 'A' || i < n - 1 && (s.charAt(i) == 'B' && s.charAt(i + 1) == 'C'))) {
+//        if (s.charAt(i) == 'A') {
+//          aCount++;
+//          i++;
+//        } else {
+//          sum += aCount;
+//          i += 2;
+//        }
+//      }
+//      j = i;
+//    }
+//    return sum;
+//  }
+
+  private static long search(String s) {
+
+    int n = s.length();
+    char[] c = s.toCharArray();
+
+    long sum = 0;
+    int j = 0;
+    while (0 <= j && j < n) {
+
+      // for each, find A
+      // found A
+      if (c[j] != 'A') {
+        j = s.indexOf("A", j + 1);
+        continue;
+      }
+
+      int aCount = 0;
+      int i = j;
+      while (i < n) {
+        if (c[i] == 'A') {
+          aCount++;
+          i++;
+        } else if (i + 1 < n && c[i] == 'B' && c[i + 1] == 'C') {
+          i += 2;
+          sum += aCount;
+        } else {
+          break;
+        }
+      }
+      j = i;
+    }
+    return sum;
+  }
+}
---
+++ agc035/src/test/java/agc035/d/MainTest.java
@@ -0,0 +1,52 @@
+package agc035.d;
+
+import agc035.d.Main;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MainTest {
+
+  @Test
+  public void test() {
+    String input = "6 4\n"
+        + "-10 8 2 1 2 6";
+
+    String expected = "14";
+
+    assertThat(execute(input + "\n"), is(expected + "\n"));
+  }
+
+  @Test
+  public void test2() {
+    String input = "6 4\n"
+        + "-6 -100 50 -2 -5 -3";
+
+    String expected = "44";
+
+    assertThat(execute(input + "\n"), is(expected + "\n"));
+  }
+
+  @Test
+  public void test3() {
+    String input = "6 3\n"
+        + "-6 -100 50 -2 -5 -3";
+
+    String expected = "0";
+
+    assertThat(execute(input + "\n"), is(expected + "\n"));
+  }
+
+  private String execute(String input) {
+    ByteArrayOutputStream os = new ByteArrayOutputStream();
+    Main.solve(
+        new ByteArrayInputStream(input.getBytes()), new PrintStream(os)
+    );
+    return os.toString().replace(System.lineSeparator(), "\n");
+  }
+}
