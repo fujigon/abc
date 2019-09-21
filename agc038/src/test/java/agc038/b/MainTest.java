e2291966c029b712835e2a21a814003d5b370132
kfujita <dec12.kaz@gmail.com>
2019-09-21 20:41:27 +0900

    prepare

---
+++ agc038/src/test/java/agc038/b/MainTest.java
@@ -0,0 +1,53 @@
+package agc038.b;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MainTest {
+
+  @Test
+  public void test() {
+    String input = "RUDLUDR";
+    String expected = "Yes";
+
+    assertThat(execute(input + "\n"), is(expected + "\n"));
+  }
+
+  @Test
+  public void test2() {
+    String input = "DULL";
+    String expected = "No";
+
+    assertThat(execute(input + "\n"), is(expected + "\n"));
+  }
+
+  @Test
+  public void test3() {
+    String input = "UUUUUUUUUUUUUUU";
+    String expected = "Yes";
+
+    assertThat(execute(input + "\n"), is(expected + "\n"));
+  }
+
+  @Test
+  public void test4() {
+    String input = "RDULULDURURLRDULRLR";
+    String expected = "Yes";
+
+    assertThat(execute(input + "\n"), is(expected + "\n"));
+  }
+
+  private String execute(String input) {
+    ByteArrayOutputStream os = new ByteArrayOutputStream();
+    Main.solve(
+        new ByteArrayInputStream(input.getBytes()), new PrintStream(os)
+    );
+    return os.toString().replace(System.lineSeparator(), "\n");
+  }
+
+}
