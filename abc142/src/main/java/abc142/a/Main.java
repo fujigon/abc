435edb40bf35df5316259e540954d879f129b025
kfujita <dec12.kaz@gmail.com>
2019-09-28 20:43:09 +0900

    prepare

---
+++ abc142/src/main/java/abc142/a/Main.java
@@ -0,0 +1,26 @@
+package abc142.a;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    solve(System.in, System.out);
+  }
+
+  static void solve(InputStream is, PrintStream os) {
+    Scanner sc = new Scanner(is);
+
+    /* read */
+    String s = sc.next();
+    if (s.equals("Sunny")) {
+      os.println("Cloudy");
+    } else if (s.equals("Cloudy")) {
+      os.println("Rainy");
+    } else {
+      os.println("Sunny");
+    }
+  }
+}
