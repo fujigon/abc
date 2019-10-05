d4209edb44daf7d9c8b8b16975ec4c34891d4b17
kfujita <dec12.kaz@gmail.com>
2019-10-05 20:50:28 +0900

    prepare

---
+++ agc039/src/main/java/agc039/d/Main.java
@@ -0,0 +1,38 @@
+package agc039.d;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    solve(System.in, System.out);
+  }
+
+  static void solve(InputStream is, PrintStream os) {
+    Scanner sc = new Scanner(is);
+
+    /* read */
+    int n = sc.nextInt();
+    int m = sc.nextInt();
+
+    Queue<Integer> queue = new PriorityQueue<>((i1, i2) -> -i1.compareTo(i2));
+
+    for (int i = 0; i < n; i++) {
+      queue.add(sc.nextInt());
+    }
+
+    for (int i = 0; i < m; i++) {
+      int max = queue.remove();
+      queue.add(max/2);
+    }
+
+    long res = 0;
+    while(!queue.isEmpty()) {
+      res += queue.remove();
+    }
+
+    os.println(res);
+  }
+}
