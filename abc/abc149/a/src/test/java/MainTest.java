a7d0bc2ab69a3d8a309997caccb1feaa75d2e77d
fujigon <dec12.kaz@gmail.com>
2020-01-03 16:48:37 +0900

    fix test

--- abc/abc149/a/src/test/java/MainTest.java
+++ abc/abc149/a/src/test/java/MainTest.java
@@ -10,29 +10,11 @@ public class MainTest {
 
   @Test
   public void test() {
-//    String input = "1\n"
-//        + "2";
-//    String expected = "3";
-//    
-//    assertThat(execute(input + "\n"), is(expected + "\n"));
+    String input = "oder atc";
+    String expected = "atcoder";
+    
+    assertThat(execute(input + "\n"), is(expected + "\n"));
   }
-
-//  @Test
-//  public void test2() {
-//    String input = "1 5 2 0";
-//    String expected = "11100";
-//
-//    assertThat(execute(input + "\n"), is(expected + "\n"));
-//  }
-//
-//
-//  @Test
-//  public void test03() {
-//    String input = "12 3 7";
-//    String expected = "0";
-//
-//    assertThat(execute(input + "\n"), is(expected + "\n"));
-//  }
   
   private String execute(String input) {
     ByteArrayOutputStream os = new ByteArrayOutputStream();
