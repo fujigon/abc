ef3103e3e6f6ee2b393f3c77ba5a8210c101b68b
fujigon <dec12.kaz@gmail.com>
2020-01-04 12:30:49 +0900

    fix

--- move.sh
+++ move.sh
@@ -5,11 +5,14 @@ for contest_type in $contest_types; do
     tasks=`ls $contest_type/$contest`
     for task in $tasks; do
       mkdir -p $contest_type/$contest/$task/src/main/java/$contest_type/$contest/$task
+      mv $contest_type/$contest/$task/src/main/java/solver/Main.java $contest_type/$contest/$task/src/main/java/$contest_type/$contest/$task/Main.java
       sed -i '1,1d' $contest_type/$contest/$task/src/main/java/$contest_type/$contest/$task/Main.java
+      echo "package $contest_type.$contest.$task;" > $contest_type/$contest/$task/src/main/java/$contest_type/$contest/$task/Main.java_
       cat $contest_type/$contest/$task/src/main/java/$contest_type/$contest/$task/Main.java >> $contest_type/$contest/$task/src/main/java/$contest_type/$contest/$task/Main.java_
       mv $contest_type/$contest/$task/src/main/java/$contest_type/$contest/$task/Main.java_ $contest_type/$contest/$task/src/main/java/$contest_type/$contest/$task/Main.java
 
       mkdir -p $contest_type/$contest/$task/src/test/java/$contest_type/$contest/$task
+      mv $contest_type/$contest/$task/src/test/java/solver/MainTest.java $contest_type/$contest/$task/src/test/java/$contest_type/$contest/$task/MainTest.java
       sed -i '1,1d' $contest_type/$contest/$task/src/test/java/$contest_type/$contest/$task/MainTest.java
       echo "package $contest_type.$contest.$task;" > $contest_type/$contest/$task/src/test/java/$contest_type/$contest/$task/MainTest.java_
       cat $contest_type/$contest/$task/src/test/java/$contest_type/$contest/$task/MainTest.java >> $contest_type/$contest/$task/src/test/java/$contest_type/$contest/$task/MainTest.java_
