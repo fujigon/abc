e659a10ba93b3656e78f08a15315280228d304b3
fujigon <dec12.kaz@gmail.com>
2020-01-04 21:20:48 +0900

    template

---
+++ abc/abc150/c/src/main/java/com/solver/atcoder/abc/abc150/c/Main.java
@@ -0,0 +1,22 @@
+package com.solver.atcoder.abc.abc150.c;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    solve(System.in, System.out);
+  }
+
+  static void solve(InputStream is, PrintStream os) {
+    Scanner sc = new Scanner(is);
+
+    /* read */
+    String s = sc.next();
+    String t = sc.next();    
+    
+    os.println(t + s);
+  }
+}
