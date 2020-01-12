b63b7935582bdfab5880c2fad4afcd8192537f4f
fujigon <dec12.kaz@gmail.com>
2020-01-13 03:02:17 +0900

    done

---
+++ abc/abc151/d/src/test/java/com/solver/atcoder/abc/abc151/d/MainTest.java
@@ -0,0 +1,44 @@
+package com.solver.atcoder.abc.abc151.d;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MainTest {
+
+  @Test
+  public void test() {
+    String input = "3 3\n"
+        + "...\n"
+        + "...\n"
+        + "...";
+    String expected = "4";
+    
+    assertThat(execute(input + "\n"), is(expected + "\n"));
+  }
+
+  @Test
+  public void test1() {
+    String input = "3 5\n"
+        + "...#.\n"
+        + ".#.#.\n"
+        + ".#...";
+    String expected = "10";
+
+    assertThat(execute(input + "\n"), is(expected + "\n"));
+  }
+
+  private String execute(String input) {
+    ByteArrayOutputStream os = new ByteArrayOutputStream();
+    Main.solve(
+        new ByteArrayInputStream(input.getBytes()), new PrintStream(os)
+    );
+    return os.toString().replace(System.lineSeparator(), "\n");
+  }
+
+}
